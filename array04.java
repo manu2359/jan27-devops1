09559f90a621d6ddcbac234ab88d5fa4a1ddc5c1
manu2359 <manoharsitaram@gmail.com>
2020-01-29 11:19:11 +0530

    2nd

---
+++ array04.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class array04 {
+	public static void main(String[] args) {
+		int bill [] = {55,85,95,55,88,55,525,66,44};
+		for(int i=0;i<=8;i++) {
+			if( bill[i]%2==0 ) {
+				
+			System.out.println(bill[i]);
+	}
+		}
+		
+	}
+
+}
+
+
